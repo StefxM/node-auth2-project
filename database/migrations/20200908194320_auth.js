4743b42dcb6c3e00babf9b520d41c8ffa5353a50
stephanie mairena <stefyem7711@gmail.com>
2020-09-13 00:03:32 -0400

    mvp?

---
+++ database/migrations/20200908194320_auth.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+      table.increments()
+      table.text('username')
+        .notNullable()
+        .unique();
+        table.text('password')
+        .notNullable()
+        table.string('department')
+        .notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users')
+};
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const usersRouter = require("./users/usersRouter")
 
 const server = express()
 const port = process.env.PORT || 5000
 
+server.use(helmet())
+server.use(cors())
 server.use(express.json())
+server.use(cookieParser())
+
+server.use(usersRouter)
 
 server.listen(port, () => {
-    console.log(`Runnig at http://localhost:${port}`)
+    console.log(`Running at http://localhost:${port}`)
 })
